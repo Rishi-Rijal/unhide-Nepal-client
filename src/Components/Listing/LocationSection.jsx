c22ff8791409958ae76e8726a395d9f8e8aa0b91
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-16 10:09:42 +0100

    updated the location box with map

--- src/Components/Listing/LocationSection.jsx
+++ src/Components/Listing/LocationSection.jsx
@@ -1,12 +1,23 @@
 import { Navigation } from "lucide-react";
+import MapView from "../../Map.jsx";
 
-const LocationSection = ({ locationImage, title }) => (
+const handleDirection = (latitude, longitude) => {
+  const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
+  window.open(url, "_blank");
+
+}
+
+const LocationSection = ({ latitude, longitude, title }) => (
   <section className="py-6 max-w-5xl mx-auto px-4">
     <h3 className="text-base font-semibold text-slate-900">Location</h3>
     <div className="mt-3 overflow-hidden rounded-2xl bg-slate-50 ring-1 ring-slate-200">
-      <img src={locationImage} alt="Map" className="h-72 w-full object-cover" />
+      <MapView
+        mode="view"
+        lat={latitude || 27.7172}
+        lng={longitude || 85.324}
+      ></MapView>
       <div className="p-3">
-        <button className="inline-flex items-center gap-2 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700">
+        <button onClick={() => handleDirection(latitude, longitude)} className="inline-flex items-center gap-2 rounded-lg bg-blue-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700">
           <Navigation className="h-4 w-4" /> Get Directions to {title}
         </button>
       </div>
