14092eb4270102ee0d2a8a3d8301c5ea1f26819c
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-17 23:29:46 +0100

    minor fix

--- src/api/listing.api.js
+++ src/api/listing.api.js
@@ -1,20 +1,16 @@
 import { diff } from "maplibre-gl";
 import api from "./axios.api.js";
 
-// export const getUser = () => api.get("/user");
-// export const loginUser = (data) => api.post("/auth/login", data);
-// export const updateUser = (data) => api.put("/user/update", data);
-
 const LISTINGS_API_BASE = "api/v1/listing";
 
 const getListings = async () => {
-    try {
-        const response = await api.get(`/${LISTINGS_API_BASE}/all`);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching listings:", error);
-        throw error;
-    }
+  try {
+    const response = await api.get(`/${LISTINGS_API_BASE}/all`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching listings:", error);
+    throw error;
+  }
 }
 
 const getFilteredListings = async ({
@@ -75,56 +71,80 @@ const getFilteredListings = async ({
 
 
 const createListing = async ({
-    name,
-    description,
-    categories,
-    tags,
-    latitude,
-    longitude,
-    tips,
-    photos,
+  name,
+  description,
+  categories,
+  tags,
+  latitude,
+  longitude,
+  tips,
+  photos,
 }) => {
-    try {
-        const formData = new FormData();
+  try {
+    const formData = new FormData();
 
-        formData.append("name", name);
-        formData.append("description", description);
-        formData.append("latitude", latitude);
-        formData.append("longitude", longitude);
+    formData.append("name", name);
+    formData.append("description", description);
+    formData.append("latitude", latitude);
+    formData.append("longitude", longitude);
 
-        categories.forEach((c) => formData.append("categories", c));
-        tags.forEach((t) => formData.append("tags", t));
+    categories.forEach((c) => formData.append("categories", c));
+    tags.forEach((t) => formData.append("tags", t));
 
-        formData.append("tipsPermits", tips.permits);
-        formData.append("tipsBestSeason", tips.bestSeason);
-        formData.append("tipsDifficulty", tips.difficulty);
-        formData.append("tipsExtra", tips.extra);
+    formData.append("tipsPermits", tips.permits);
+    formData.append("tipsBestSeason", tips.bestSeason);
+    formData.append("tipsDifficulty", tips.difficulty);
+    formData.append("tipsExtra", tips.extra);
 
-        photos.forEach((file) => {
-            formData.append("images", file);
-        });
+    photos.forEach((file) => {
+      formData.append("images", file);
+    });
 
-        const response = await api.post(`${LISTINGS_API_BASE}/all`, formData, {
-            headers: { "Content-Type": "multipart/form-data" },
-        });
+    const response = await api.post(`${LISTINGS_API_BASE}/all`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
 
-        return response.data;
-    } catch (error) {
-        console.error("Error creating listing:", error);
-        throw error;
-    }
+    return response.data;
+  } catch (error) {
+    console.error("Error creating listing:", error);
+    throw error;
+  }
 };
 
 const getListing = async (id) => {
-    try {
-        const response = await api.get(`${LISTINGS_API_BASE}/${id}`);
-        return response;
-    } catch (error) {
-        throw error
-    }
+  try {
+    const response = await api.get(`${LISTINGS_API_BASE}/${id}`);
+    return response;
+  } catch (error) {
+    throw error
+  }
+}
+
+const addLike = async (id) => {
+  try {
+    const response = await api.post(`${LISTINGS_API_BASE}/${id}/like`);
+    return response.data;
+
+  } catch (error) {
+    throw error;
+  }
+}
+
+const removeLike = async (id) => {
+  try {
+    const response = await api.patch(`${LISTINGS_API_BASE}/${id}/like`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 
-
-
-export { getListings, getFilteredListings, createListing, getListing };
+export {
+  getListings,
+  getFilteredListings,
+  createListing,
+  getListing,
+  addLike,
+  removeLike
+};
