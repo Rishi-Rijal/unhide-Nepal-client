b7cc1d9a7c0fc75ad90e99213585aaa58a54d0c9
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-22 12:43:02 +0200

    updated the container

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,5 +1,7 @@
 const Container = ({ children, className = "" }) => (
-  <div className={`mx-auto w-full max-w-6xl px-4 sm:px-6 lg:px-8 ${className}`}>{children}</div>
+  <div className={`mx-auto w-full max-w-7xl 2xl:max-w-screen-2xl px-4 sm:px-6 lg:px-8 ${className}`}>
+    {children}
+  </div>
 );
 
 export default Container;
