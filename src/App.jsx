e36b78ccbf6ea1096d079569b92b8d15240b1b6b
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-22 12:40:36 +0200

    added authentication for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
@@ -12,27 +11,76 @@ import Listing from './pages/Listing.jsx'
 import LoginPage from './pages/Login.jsx'
 import RegisterPage from './pages/Register.jsx'
 import Wishlist from './pages/Wishlist.jsx'
+import ChangePassword from './pages/ChangePassword.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
+import ForgotPassword from './pages/ForgotPassword.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './Components/Layout/Layout.jsx';
+import RequireAuth from './Components/RequireAuth.jsx';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setUser, clearUser } from './utils/authSlice.js';
+import { getUserProfile } from './api/user.api.js'
+import { refreshAccessToken } from './api/user.api.js'
+import AuthSuccess from './pages/AuthSuccess.jsx'
 
 const App = () => {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+useEffect(() => {
+  const fetchUserProfile = async (options = { retry: true }) => {
+    try {
+      const userProfile = await getUserProfile();
+      dispatch(setUser(userProfile.data.user));
+      return userProfile;
+    } catch (err) {
+      const status = err?.response?.status;
+      const isAuthFailure = status === 401;
+
+      if (isAuthFailure && options.retry) {
+        try {
+          await refreshAccessToken(); 
+          const retried = await getUserProfile();
+          dispatch(setUser(retried.data.user));
+          return retried;
+        } catch (refreshErr) {
+          dispatch(clearUser());
+          throw refreshErr;
+        }
+      }
+
+      dispatch(clearUser());
+      throw err;
+    }
+  };
+
+  if (user === undefined) {
+    fetchUserProfile();
+  }
+}, [dispatch, user]);
+
   return (
     <BrowserRouter>
       <Routes>
         {/* Parent Route with Outlet */}
         <Route path="/" element={<Layout />}>
           {/* Child routes */}
-          <Route index element={<Home />} />   
-          <Route path="/About" element={<About />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-          <Route path="/TermsOfService" element={<TermsOfService />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Explore" element={<ExplorePlaces />} />
-          <Route path="/Listing/New" element={<NewListing />} />
-          <Route path="/Listing/:id" element={<Listing />} />
-          <Route path="/Wishlist" element={<Wishlist />} />
-          <Route path="/Login" element={<LoginPage />} />
-          <Route path="/Register" element={<RegisterPage />} />
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/explore" element={<ExplorePlaces />} />
+          <Route path="/listing/new" element={<RequireAuth><NewListing /></RequireAuth>} />
+          <Route path="/listing/:id" element={<Listing />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/change-password" element={<RequireAuth><ChangePassword /></RequireAuth>} />
+          <Route path="/reset-password/:id/:token" element={<ResetPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/auth/success" element={<AuthSuccess />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
