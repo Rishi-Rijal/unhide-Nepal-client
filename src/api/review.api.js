cb50859840f8a6003eddaae41d9f9a8d7f636b30
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-22 12:41:44 +0200

    fixed bugs

--- src/api/review.api.js
+++ src/api/review.api.js
@@ -4,45 +4,29 @@ const RATING_API_BASE = "api/v1/review";
 
 
 const getRatingsByListingID = async (postId) => {
-    try {
-        const response = await api.get(`${RATING_API_BASE}/${postId}`)
-        return response.data
-    } catch (error) {
-        throw error
-    }
+    const response = await api.get(`${RATING_API_BASE}/${postId}`);
+    return response.data;
 }
 
 const addUserReview = async ({ postId, author, rating, text }) => {
-    try {
-        const response = await api.post(`${RATING_API_BASE}/${postId}`,
-            {
-                userName: author,
-                rating,
-                reviewMsg: text
-            }
-        );
-        return response.data;
-    } catch (error) {
-        throw error
-    }
+    const response = await api.post(`${RATING_API_BASE}/${postId}`,
+        {
+            userName: author,
+            rating,
+            reviewMsg: text
+        }
+    );
+    return response.data;
 }
 
 const updateReview = async (reviewId, { rating, reviewMsg }) => {
-    try {
-        const response = await api.patch(`${RATING_API_BASE}/${reviewId}`, { rating, reviewMsg });
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+    const response = await api.patch(`${RATING_API_BASE}/${reviewId}`, { rating, reviewMsg });
+    return response.data;
 }
 
 const deleteReview = async (reviewId) => {
-    try {
-        const response = await api.delete(`${RATING_API_BASE}/${reviewId}`);
-        return response.data;
-    } catch (error) {
-        throw error;
-    }
+    const response = await api.delete(`${RATING_API_BASE}/${reviewId}`);
+    return response.data;
 }
 
 export { getRatingsByListingID, addUserReview, updateReview, deleteReview }
