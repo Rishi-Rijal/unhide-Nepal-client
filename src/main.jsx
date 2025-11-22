c53cedd2af4eeae0338a90e81f3290c2ca0340d2
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-22 12:40:57 +0200

    added the redux store for auth state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ToastProvider } from './Components/Shared/Toast'
+import { Provider } from 'react-redux'
+import store from './utils/store.js'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToastProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ToastProvider>
   </StrictMode>,
 )
