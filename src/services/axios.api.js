a0835a95b9fbda29510019039ff5feb4c2e516d0
Rishi <ramrijalrishi@gmail.com>
2025-11-29 14:17:02 +0200

    api bug fix

--- src/services/axios.api.js
+++ src/services/axios.api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const isProd = Boolean(import.meta.env.VITE_IS_PROD);
-const configuredBase = import.meta.env.VITE_APP_API_BASE_URL;
+const configuredBase = import.meta.env.VITE_SERVER_URI;
+console.log("Configured Base URL:", configuredBase);
 const baseURL = configuredBase ?? (isProd ? window.location.origin : "http://localhost:8000");
 
 const api = axios.create({
