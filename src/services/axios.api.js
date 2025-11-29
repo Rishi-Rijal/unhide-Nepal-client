2eb0a3f5108f06ae4fdfb14a5897d36ae7117d9f
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-11-29 18:52:11 +0100

    added api for the map

--- src/services/axios.api.js
+++ src/services/axios.api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const isProd = Boolean(import.meta.env.VITE_IS_PROD);
 const configuredBase = import.meta.env.VITE_SERVER_URI;
 const baseURL = configuredBase ?? (isProd ? window.location.origin : "http://localhost:8000");
-
 const api = axios.create({
   baseURL,
   withCredentials: true,
